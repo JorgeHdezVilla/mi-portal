ea8ef8f376e5ad3ee0ff72e2c72df61e635e0a14
Jorge Hdez <jorge@gmai.com>
2026-02-10 08:31:07 -0600

    remove_host

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "dev-insecure")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "0") == "1"
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost,127.0.0.1,mi-portal-3zw4.onrender.com").split(",")
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost,127.0.0.1.").split(",")
 
 
 # Application definition
